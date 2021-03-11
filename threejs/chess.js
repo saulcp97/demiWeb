dbef687d7c03246430308d9d40ea4fb67644eda5
saulcp97 <saulcp97@gmail.com>
2021-03-11 01:05:09 +0100

    Tabletop

--- threejs/chess.js
+++ threejs/chess.js
@@ -3,16 +3,20 @@
 //meter:
 //https://threejs.org/docs/#manual/en/introduction/Loading-3D-models
 
+
+const TABLETOPSIZE = 8
 var renderer, scene, camera, cubo;
 var cameraControls;
 var angulo = -0.01;
+var tablero = new Array(TABLETOPSIZE)
 
 init();
-loadModel(1.0);
+loadTablero();
+loadPieces();
+//loadModel(1.0);
 render();
 
-function init()
-{
+function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.setClearColor( new THREE.Color(0x7777FF) );
@@ -30,6 +34,33 @@ function init()
   window.addEventListener('resize', updateAspectRatio );
 }
 
+function loadTablero() {
+	for(let i = 0; i < TABLETOPSIZE; ++i) {
+		tablero[i] = new Array(TABLETOPSIZE);
+		for(let j = 0; j < TABLETOPSIZE; ++j) {
+			tablero[i][j] = null;
+		}
+	}
+}
+
+
+function loadPieces() {
+	for(let i = 0; i < tablero.length; ++i) {
+		for(let j = 0; j < tablero[i].length; ++j) {
+			
+			const geometry = new THREE.BoxGeometry( 1, 1, 1);
+			// Configura un material
+			var textura = new THREE.ImageUtils.loadTexture( 'images/ilovecg.png' );
+			var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors, map: textura, side: THREE.DoubleSide } );
+			// Construye el objeto grafico 
+			tablero[i][j] = new THREE.Mesh(geometry, material );
+			// Añade el objeto grafico a la escena
+			tablero[i][j].position.set(i*1.1, 0,  j*1.1)
+			scene.add(tablero[i][j]);
+		}
+	}
+}
+
 function loadModel(lado) {
 	const geometry = new THREE.BoxGeometry( lado, lado, lado);
 	// Configura un material
@@ -40,13 +71,13 @@ function loadModel(lado) {
 	// Añade el objeto grafico a la escena
 	scene.add( cubo );
 	
-	const loader = new THREE.ObjectLoader();
+	/*const loader = new THREE.ObjectLoader();
 
-	loader.load( 'models/soldado/soldado.json', function ( gltf ) {
+	//loader.load( 'models/soldado/soldado.json', function ( gltf ) {
 		scene.add( gltf.scene );
 	}, undefined, function ( error ) {
 		console.error( error );
-	});
+	});*/
 }
 
 function updateAspectRatio()
@@ -62,7 +93,7 @@ function update()
   cameraControls.update();
 
   // Movimiento propio del cubo
-	cubo.rotateOnAxis( new THREE.Vector3(0,1,0), angulo );
+	//cubo.rotateOnAxis( new THREE.Vector3(0,1,0), angulo );
 }
 
 function render()
